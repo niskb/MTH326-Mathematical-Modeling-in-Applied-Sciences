69ec2794bae8dced8ae78dc171bbf25da87b2b19
Brian Niski <50961607+niskb@users.noreply.github.com>
2025-01-28 11:04:40 -0500

    In-class MATLAB Programming Assignment 1

---
+++ Assignments/In-Class/MATLAB/1/MATLAB_assignment_1.m
@@ -0,0 +1,6 @@
+% MATLAB assignment 1
+% Instruction:
+% 1. First, display your name.
+% 2. Write a formula that converts Celsius to Fahrenheit in a function
+% 3. Have an input in degree C then ouput it in degree F using fprintf
+
